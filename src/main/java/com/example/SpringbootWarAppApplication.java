104ccdd1e4bb674b06cf0fcc16b1c38ad6871830
Abhishek verma <86115498+AV311241@users.noreply.github.com>
2025-05-09 23:07:03 +0530

    first commit

---
+++ src/main/java/com/example/SpringbootWarAppApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootWarAppApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWarAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootWarAppApplication.class);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+
+	@GetMapping("")
+	public String home() {
+		return "My Spring boot War app is home page Abhishek verma";
+	}
+	
+	
+	@GetMapping("/test")
+	public String test() {
+		return "My Spring boot War app is Running";
+	}
+	
+}
